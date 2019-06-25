95a3f2985568f800252a502136d32a9402b3e846
shillwil <alex.shillingford@icloud.com>
2019-06-25 10:52:35 -0600

    Committing

---
+++ Tipsease/Tipsease/Bearer.swift
@@ -0,0 +1,13 @@
+//
+//  Bearer.swift
+//  Tipsease
+//
+//  Created by Alex Shillingford on 6/25/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+struct Bearer: Codable {
+    var token: String
+}
---
+++ Tipsease/Tipsease/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  Tipsease
+//
+//  Created by Alex Shillingford on 6/25/19.
+//  Copyright © 2019 Marlon Raskin. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    var username: String
+    var password: String
+}
