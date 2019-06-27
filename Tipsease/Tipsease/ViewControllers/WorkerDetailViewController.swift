beb8a81bc69b247552c4b1e267523dec4307a451
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-26 22:20:46 -0400

    In the middle of implementing code to get tip function to work on worker detail page. It's not working yet, but I need Alex's model to finish.

--- Tipsease/Tipsease/ViewControllers/WorkerDetailViewController.swift
+++ Tipsease/Tipsease/ViewControllers/WorkerDetailViewController.swift
@@ -12,6 +12,7 @@ class WorkerDetailViewController: UIViewController {
 
 	var workerController: WorkerController?
 	var worker: Worker?
+	var amountTypedString2 = ""
 	
 	@IBOutlet var workerProfileImage: UIImageView!
 	@IBOutlet var leaveTipButton: UIButton!
@@ -55,3 +56,57 @@ class WorkerDetailViewController: UIViewController {
     */
 
 }
+
+
+extension WorkerDetailViewController: UITextFieldDelegate {
+	func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+		let formatter = NumberFormatter()
+		formatter.locale = Locale.current
+		formatter.minimumFractionDigits = 2
+		formatter.maximumFractionDigits = 2
+		
+		if string.count > 0 {
+			amountTypedString2 += string
+			let decNumber = NSDecimalNumber(string: amountTypedString2).multiplying(by: 0.01)
+			let newString = "$" + formatter.string(from: decNumber)!
+			textField.text = newString
+		} else {
+			amountTypedString2 = String(amountTypedString2.dropLast())
+			if amountTypedString2.count > 0 {
+				let decNumber = NSDecimalNumber(string: amountTypedString2).multiplying(by: 0.01)
+				let newString = "$" +  formatter.string(from: decNumber)!
+				textField.text = newString
+			} else {
+				textField.text = "$0.00"
+			}
+		}
+		return false
+	}
+}
+
+extension WorkerDetailViewController: UIAlertController {
+
+		self.amountTypedString = ""
+		let tipAlertController = UIAlertController(title: "How much would you like to tip \(self.workerController.workers[indexPath.row].name)?", message: nil, preferredStyle: .alert)
+		tipAlertController.addTextField(configurationHandler: { (tipTextField) in
+			tipTextField.delegate = self
+			tipTextField.placeholder = "Enter tip amount"
+			tipTextField.keyboardType = .numberPad
+			let tipCancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+			let tipActionButton = UIAlertAction(title: "Tip", style: .default, handler: { (tipUIAlertAction) in
+				guard let tipFromTextField = tipTextField.text else { return }
+				let secondAlert = UIAlertController(title: "Are you sure \(String(describing: tipFromTextField)) is the correct amount that you want to tip?", message: "", preferredStyle: .alert)
+				let secondAlertNoAction = UIAlertAction(title: "No", style: .destructive, handler: nil)
+				let secondAlertYesAction = UIAlertAction(title: "Yes", style: .default, handler: nil)
+				secondAlert.addAction(secondAlertYesAction)
+				secondAlert.addAction(secondAlertNoAction)
+				present(secondAlert, animated: true, completion: nil)
+
+			})
+			tipAlertController.addAction(tipCancelButton)
+			tipAlertController.addAction(tipActionButton)
+			self.present(tipAlertController, animated: true, completion: nil)
+		})
+
+}
+
