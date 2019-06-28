e480557a8578f8ac3033d7d34ad00f377c44aff8
Marlon Raskin <marlonjraskin@gmail.com>
2019-06-27 20:28:44 -0400

    Added app icon

--- Tipsease/Tipsease/ViewControllers/WorkerVCs/WorkerDetailViewController.swift
+++ Tipsease/Tipsease/ViewControllers/WorkerVCs/WorkerDetailViewController.swift
@@ -30,7 +30,7 @@ class WorkerDetailViewController: UIViewController, UITextFieldDelegate {
 	@IBOutlet var tipAmountThreeLabel: UILabel!
 	@IBOutlet var taglineTextView: UITextView!
 	
-	let layerGradient = CALayerGra
+	let layerGradient = CAGradientLayer()
 	
 	
 	override func viewDidLoad() {
@@ -48,15 +48,14 @@ class WorkerDetailViewController: UIViewController, UITextFieldDelegate {
 			for location in workerObject.locations {
 				locationStr.append(location.name)
 			}
-			
-			
 		}
 		
-		layerGradient.colors = [Colors.secondaryBlue.cgColor, Colors.deeperBlue.cgColor]
-		layerGradient.startPoint = CGPoint(x: 0, y: 0.5)
-		layerGradient.endPoint = CGPoint(x: 1, y: 0.5)
-		layerGradient.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
-		tabBarController?.tabBar.layer.addSublayer(layerGradient)
+		//TODO: - Gradient Button
+//		layerGradient.colors = [Colors.secondaryBlue.cgColor, Colors.deeperBlue.cgColor]
+//		layerGradient.startPoint = CGPoint(x: 0, y: 0.5)
+//		layerGradient.endPoint = CGPoint(x: 1, y: 0.5)
+//		layerGradient.frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height)
+//		leaveTipButton.layer.addSublayer(layerGradient)
 		
     }
     
